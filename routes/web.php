5bd2f10c14aa1c6691990089082bd50723b48ceb
sndp bag <sndpbagg@gmail.com>
2025-07-20 19:56:43 +0530

    role route make

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\productController;
+use App\Http\Controllers\roleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -14,3 +15,4 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource("users",UserController::class);
 Route::resource("products",productController::class);
+Route::resource("roles",roleController::class);
