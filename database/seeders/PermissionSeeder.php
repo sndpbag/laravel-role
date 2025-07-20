8474fa08a46d876a6dc07d8325431d2bd49027ab
sndp bag <sndpbagg@gmail.com>
2025-07-20 19:55:32 +0530

    create roll controller

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
 
 class PermissionSeeder extends Seeder
 {
@@ -13,5 +14,20 @@ class PermissionSeeder extends Seeder
     public function run(): void
     {
         //
+
+        $permissions = [
+            "role-list",
+            "role-edit",
+            "role-create",
+            "role-delete",
+            "product-list",
+            "product-edit",
+            "product-create",
+            "product-delete"
+        ];
+
+       foreach ($permissions as $key => $permission) {
+        Permission::create(['name' =>  $permission]);
+       }
     }
 }
